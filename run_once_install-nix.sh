79362ee2f7635b556c0e06bb36d9100476057ea6
Beforerr <zzj956959688@gmail.com>
2023-02-19 22:58:38 -0800

    Use scripts to perform actions

--- run_once_install-nix.sh
+++ run_once_install-nix.sh
@@ -1,1 +1,10 @@
-which nix || curl -L https://nixos.org/nix/install | sh
+#!/bin/bash
+if ! command -v nix &> /dev/null; then
+	curl -L https://nixos.org/nix/install | sh
+fi
+
+if ! command -v home-manager &> /dev/null; then
+	nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+	nix-channel --update 
+	nix-shell '<home-manager>' -A install
+fi
