7206bccd61176ca0a74fffe4db3f100d6e7f9628
Beforerr <zzj956959688@gmail.com>
2023-02-19 23:03:11 -0800

    Update install-nix.sh

--- run_onchange_install-nix.sh
+++ run_onchange_install-nix.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if ! command -v nix &> /dev/null; then
+if ! command -v nix; then
 	curl -L https://nixos.org/nix/install | sh
 fi
 
-if ! command -v home-manager &> /dev/null; then
+if ! command -v home-manager; then
 	nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 	nix-channel --update 
 	nix-shell '<home-manager>' -A install
