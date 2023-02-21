6282b1eec68c04874d97d69aa5810245c6289d48
Beforerr <zzj956959688@gmail.com>
2023-02-20 20:21:53 -0800

    Add home/.chezmoiscripts/install-brew.sh

---
+++ home/.chezmoiscripts/run_once_after_install-brew.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing 'brew' package manager..."
+if ! command -v brew; then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | NONINTERACTIVE=1 bash
+fi
+
+echo "Installing packages with 'brew'..."
+if command -v brew; then
+    brew bundle --file="$HOME/Brewfile"
+else
+    echo "Brew is not installed. Please install it first."
+fi
