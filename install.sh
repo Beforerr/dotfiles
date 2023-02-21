66d0d9c5d67fe4e4f6196a065c84cdde226970d9
Beforerr <zzj956959688@gmail.com>
2023-02-20 21:33:44 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Installing chezmoi..."
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$bin_dir" init --apply $GITHUB_USERNAME
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$bin_dir" init --apply "Beforerr"
   else
     echo "To install chezmoi, you must have curl installed." >&2
     exit 1
