f3cfbf0ae606fa06f1a84326db0748913760d994
Beforerr <zzj956959688@gmail.com>
2023-03-01 19:25:39 -0800

    Add home/.config/zsh/plugins/aliases.zsh Add home/.config/zsh/plugins/micromamba.zsh

---
+++ home/dot_config/zsh/plugins/aliases.zsh
@@ -0,0 +1,43 @@
+# Shortcuts for common commands 
+colorflag="-G"
+alias l="ls -lF ${colorflag}"
+alias la="ls -lAF ${colorflag}"
+alias ll="ls -lAF ${colorflag}"
+alias ls="ls -G"
+
+# Easier navigation
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Directories
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias p="cd ~/projects"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Sublime
+alias s="subl"
+
+# Homebrew
+alias b="brew"
+alias bb="brew bundle"
+
+# Home manager
+alias hm="home-manager"
+alias hms="home-manager switch"
+alias hme="home-manager edit"
+
+# Chezmoi
+alias cm="chezmoi"
+alias cma="chezmoi apply"
+alias cmr="chezmoi re-add"
+alias cme="chezmoi edit"
+alias cmcd="chezmoi cd"
+
+# Micromamba
+alias m="micromamba"
+alias mamba="micromamba"
+alias conda="micromamba"
