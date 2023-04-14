1954d50035c3699fdbeb28e6fb220a2c6f51ac6f
Beforerr <zzj956959688@gmail.com>
2023-04-14 09:02:57 -0700

    Add home/.config/zsh/plugins/idl.zsh

---
+++ home/dot_config/zsh/plugins/idl.zsh
@@ -0,0 +1,11 @@
+
+
+export EXELIS_DIR=$HOME/Applications/harris
+export IDL_DIR=$EXELIS_DIR/idl88
+export IDL_EXE=$IDL_DIR/bin/idl
+
+if command -v $IDL_EXE >/dev/null; then
+    export PATH="$IDL_DIR/bin:$PATH"
+    export PYTHONPATH=$IDL_DIR/lib/bridges:$PYTHONPATH
+    export PYTHONPATH=$IDL_DIR/bin/bin.darwin.x86_64:$PYTHONPATH
+fi
