eb52641150cde5e1585ffc7c354123fa2ed1d481
Beforerr <zzj956959688@gmail.com>
2023-05-18 12:47:40 -0700

    Update home/.config/zsh/plugins/idl.zsh

--- home/dot_config/zsh/plugins/idl.zsh
+++ home/dot_config/zsh/plugins/idl.zsh
@@ -1,10 +1,10 @@
 export EXELIS_DIR=$HOME/Applications/harris
 export IDL_DIR=$EXELIS_DIR/idl88
 export IDL_EXE=$IDL_DIR/bin/idl
+export SPEDAS_DATA_DIR=$HOME/data
 
 if command -v $IDL_EXE >/dev/null; then
     export PATH="$IDL_DIR/bin:$PATH"
     export PYTHONPATH=$IDL_DIR/lib/bridges:$PYTHONPATH
     export PYTHONPATH=$IDL_DIR/bin/bin.darwin.x86_64:$PYTHONPATH
-    # export JUPYTER_PATH=$IDL_DIR/lib/bridges
 fi
