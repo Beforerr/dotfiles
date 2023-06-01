0819a47030defcae32be71afec0e7c50f2dc5f51
Beforerr <zzj956959688@gmail.com>
2023-06-01 11:05:43 -0700

    Update home/.config/zsh/plugins/micromamba.zsh

--- home/dot_config/zsh/plugins/micromamba.zsh
+++ home/dot_config/zsh/plugins/micromamba.zsh
@@ -1,16 +1,16 @@
 # >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE="/Users/zijin/.nix-profile/bin/micromamba";
-export MAMBA_ROOT_PREFIX="/Users/zijin/micromamba";
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    if [ -f "/Users/zijin/micromamba/etc/profile.d/micromamba.sh" ]; then
-        . "/Users/zijin/micromamba/etc/profile.d/micromamba.sh"
-    else
-        export  PATH="/Users/zijin/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init
-    fi
-fi
-unset __mamba_setup
+# # !! Contents within this block are managed by 'mamba init' !!
+# export MAMBA_EXE="/Users/zijin/.nix-profile/bin/micromamba";
+# export MAMBA_ROOT_PREFIX="/Users/zijin/micromamba";
+# __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__mamba_setup"
+# else
+#     if [ -f "/Users/zijin/micromamba/etc/profile.d/micromamba.sh" ]; then
+#         . "/Users/zijin/micromamba/etc/profile.d/micromamba.sh"
+#     else
+#         export  PATH="/Users/zijin/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init
+#     fi
+# fi
+# unset __mamba_setup
 # <<< mamba initialize <<<
