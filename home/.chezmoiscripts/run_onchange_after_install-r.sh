331dbcc699b9ff8d82c6361297f18e22c221d1df
Beforerr <zzj956959688@gmail.com>
2023-10-23 12:58:55 -0700

    Update home/.chezmoiscripts/install-r.sh Update home/packages.R

--- home/.chezmoiscripts/run_onchange_after_install-r.sh
+++ home/.chezmoiscripts/run_onchange_after_install-r.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# packages.R hash: {{ include "packgages.R" | sha256sum }}
-Rscript ~/packgages.R
+# packages.R hash: {{ include "packages.R" | sha256sum }}
+Rscript ~/packages.R
--- home/packages.R
+++ home/packages.R
@@ -2,3 +2,5 @@
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
 install.packages("pak")
+pak::pkg_install("tidyverse")
+pak::pkg_install("arrow")
