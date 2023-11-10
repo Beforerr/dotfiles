8806c94850a2b4a0518bc61d2af5a52af3d85081
Beforerr <zzj956959688@gmail.com>
2023-11-09 21:08:15 -0800

    Remove home/.chezmoiscripts/install-spack.sh Update home/justfile Update home/Brewfile

--- home/.chezmoiscripts/run_once_after_install-spack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing 'spack' package manager..."
-if ! command -v spack; then
-	git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-	. spack/share/spack/setup-env.sh
-fi
