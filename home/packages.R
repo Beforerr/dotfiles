6d3aa252e8133420c0cb577247da9fff43c25230
Beforerr <zzj956959688@gmail.com>
2023-12-01 21:20:41 -0800

    Update home/packages.R

--- home/packages.R
+++ home/packages.R
@@ -1,14 +1,14 @@
-
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
 install.packages("pak")
 
+pak::pkg_install("languageserver")
+
 pak::pkg_install("tidyverse")
 pak::pkg_install("arrow")
 
-list = c("ggplot2", "ggpubr", "patchwork")
+list <- c("ggplot2", "ggpubr", "patchwork")
 pak::pkg_install(list)
 
-
-pak::repo_add(rhub = 'https://easystats.r-universe.dev')
+pak::repo_add(rhub = "https://easystats.r-universe.dev")
 pak::pkg_install("easystats/easystats")
