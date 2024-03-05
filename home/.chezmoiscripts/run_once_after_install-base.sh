0a32756fa52d8ea815d15a3c913b9f38fdb20b84
Beforerr <zzj956959688@gmail.com>
2024-03-04 20:48:58 -0800

    Update home/.chezmoiscripts/install-base.sh Update home/.bashrc Add home/.zshrc Change attributes of home/.zshrc

--- home/.chezmoiscripts/run_once_after_install-base.sh
+++ home/.chezmoiscripts/run_once_after_install-base.sh
@@ -5,6 +5,7 @@ echo "Installing 'miniforge' package manager..."
 if ! command -v conda; then
 	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 	bash Miniforge3-$(uname)-$(uname -m).sh -b
+	rm Miniforge3-$(uname)-$(uname -m).sh
 fi
 
 # Notes: 
