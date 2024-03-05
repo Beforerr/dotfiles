928558a7dd2dd9ac4a0956e7dccc3c5bc2317da2
Beforerr <zzj956959688@gmail.com>
2024-03-04 20:55:37 -0800

    Update home/.bashrc_linux Update home/.chezmoiscripts/install-pipx.sh

--- home/.chezmoiscripts/run_onchange_after_install-pipx.sh
+++ home/.chezmoiscripts/run_onchange_after_install-pipx.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+if command -v pipx &> /dev/null; then
+    # mackup
+    pipx install mackup
 
-# mackup
-pipx install mackup
+    # Python environment
+    pipx install nbdev
+    pipx install nbqa
+    pipx install black isort
+    pipx inject nbqa isort
+    pipx inject nbqa black
 
-# Python environment
-pipx install nbdev
-pipx install nbqa
-pipx install black isort
-pipx inject nbqa isort
-pipx inject nbqa black
+    pipx install thefuck
 
-pipx install thefuck
-
-pipx install segno # generate qr code
+    pipx install segno # generate qr code
+fi
