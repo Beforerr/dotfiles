8386557ced9b3e50643db98b0fa9254a1b33c56e
Beforerr <zzj956959688@gmail.com>
2024-04-27 13:24:39 -0700

    Update home/.config/home-manager/home.nix

--- home/dot_config/home-manager/home.nix
+++ home/dot_config/home-manager/home.nix
@@ -27,6 +27,7 @@
     # pkgs.pipx # Note: outdated
     pkgs.temurin-bin
     pkgs.ruby
+    # pkgs.rustup
     # pkgs.nodejs #: The operation was rejected by your operating system.
     pkgs.pandoc
     pkgs.librsvg
