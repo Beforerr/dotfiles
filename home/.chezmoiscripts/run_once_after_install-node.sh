c51d62c30493fa296db9debdc86a84674c44c53e
Beforerr <zzj956959688@gmail.com>
2024-04-27 13:12:19 -0700

    Add home/.chezmoiscripts/install-node.sh Remove home/.chezmoiscripts/install-node.sh

---
+++ home/.chezmoiscripts/run_once_after_install-node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install node
+# curl -fsSL https://get.pnpm.io/install.sh | sh -
