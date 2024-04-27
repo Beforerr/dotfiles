ba49e6cf623019e594fc9facb489dde8a5563bc5
Beforerr <zzj956959688@gmail.com>
2024-04-27 16:52:11 -0700

    Update home/.config/zsh/plugins/aliases.zsh

--- home/dot_config/zsh/plugins/aliases.zsh
+++ home/dot_config/zsh/plugins/aliases.zsh
@@ -29,8 +29,10 @@ alias p="cd ~/projects"
 # Recursively delete `.DS_Store` files
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
-# Sublime
+# Editors
+alias c="code"
 alias s="subl"
+alias v="nvim"
 
 # Homebrew
 alias b="brew"
