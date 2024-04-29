6a6c48d1f20c9ac805739d3205022630e8f1e4d5
Beforerr <zzj956959688@gmail.com>
2024-04-28 23:26:31 -0700

    Update home/.chezmoiscripts/install-base.sh Update home/.zshrc

--- home/.chezmoiscripts/run_once_after_install-base.sh
+++ home/.chezmoiscripts/run_once_after_install-base.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Notes: With the release of Miniforge3-22.3.1-0, that incorporated the changes in #277, the packages and configuration of Mambaforge and Miniforge3 are now identical. 
-if ! command -v conda; then
-	echo "Installing 'miniforge' package manager..."
-	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-	bash Miniforge3-$(uname)-$(uname -m).sh -b
-	rm Miniforge3-$(uname)-$(uname -m).sh
-fi
+# if ! command -v conda; then
+# 	echo "Installing 'miniforge' package manager..."
+# 	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+# 	bash Miniforge3-$(uname)-$(uname -m).sh -b
+# 	rm Miniforge3-$(uname)-$(uname -m).sh
+# fi
 
 # Notes: 
 if ! command -v micromamba; then
