6370d70e62904d5ad28f2ef9f1ccb88236b7a4ab
Beforerr <zzj956959688@gmail.com>
2024-04-29 20:50:41 -0700

    Update home/.chezmoiscripts/install-base.sh Update home/.chezmoiscripts/install-node.sh Update home/.zshrc

--- home/.chezmoiscripts/run_once_after_install-base.sh
+++ home/.chezmoiscripts/run_once_after_install-base.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Notes: With the release of Miniforge3-22.3.1-0, that incorporated the changes in #277, the packages and configuration of Mambaforge and Miniforge3 are now identical. 
-# if ! command -v conda; then
-# 	echo "Installing 'miniforge' package manager..."
-# 	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-# 	bash Miniforge3-$(uname)-$(uname -m).sh -b
-# 	rm Miniforge3-$(uname)-$(uname -m).sh
-# fi
-
-# Notes: 
 if ! command -v micromamba; then
 	echo "Installing 'micromamba' package manager..."
 	"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
+fi
+
+# Notes: With the release of Miniforge3-22.3.1-0, that incorporated the changes in #277, the packages and configuration of Mambaforge and Miniforge3 are now identical. 
+function _install_conda() {
+	echo "Installing 'conda' package manager..."
+	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+	bash Miniforge3-$(uname)-$(uname -m).sh -b
+	rm Miniforge3-$(uname)-$(uname -m).sh
+}
+
+if [[ $INSTALL_CONDA ]]; then
+	command -v conda || install_conda
 fi
