b198e524a47104c6567baddf71fb90d6f6e48875
Beforerr <zzj956959688@gmail.com>
2024-08-31 18:54:21 -0700

    Add home/nixos-config/apps/aarch64-darwin/apply Add home/nixos-config/apps/aarch64-darwin/build Add home/nixos-config/apps/aarch64-darwin/build-switch Add home/nixos-config/apps/aarch64-darwin/rollback Add home/nixos-config/apps/aarch64-linux Add home/nixos-config/apps/x86_64-darwin/apply Add home/nixos-config/apps/x86_64-darwin/build Add home/nixos-config/apps/x86_64-darwin/build-switch Add home/nixos-config/apps/x86_64-darwin/check-keys Add home/nixos-config/apps/x86_64-darwin/copy-keys Add home/nixos-config/apps/x86_64-darwin/create-keys Add home/nixos-config/apps/x86_64-linux/apply Add home/nixos-config/apps/x86_64-linux/build-switch Add home/nixos-config/flake.lock Add home/nixos-config/flake.nix Add home/nixos-config/hosts/darwin/default.nix Add home/nixos-config/hosts/nixos/default.nix Add home/nixos-config/modules/darwin/README.md Add home/nixos-config/modules/darwin/casks.nix Add home/nixos-config/modules/darwin/dock/default.nix Add home/nixos-config/modules/darwin/files.nix Add home/nixos-config/modules/darwin/home-manager.nix Add home/nixos-config/modules/darwin/packages.nix Add home/nixos-config/modules/nixos/README.md Add home/nixos-config/modules/nixos/config/login-wallpaper.png Add home/nixos-config/modules/nixos/config/polybar/bars.ini Add home/nixos-config/modules/nixos/config/polybar/colors.ini Add home/nixos-config/modules/nixos/config/polybar/config.ini Add home/nixos-config/modules/nixos/config/polybar/modules.ini Add home/nixos-config/modules/nixos/config/polybar/user_modules.ini Add home/nixos-config/modules/nixos/config/rofi/colors.rasi Add home/nixos-config/modules/nixos/config/rofi/confirm.rasi Add home/nixos-config/modules/nixos/config/rofi/launcher.rasi Add home/nixos-config/modules/nixos/config/rofi/message.rasi Add home/nixos-config/modules/nixos/config/rofi/networkmenu.rasi Add home/nixos-config/modules/nixos/config/rofi/powermenu.rasi Add home/nixos-config/modules/nixos/config/rofi/styles.rasi Add home/nixos-config/modules/nixos/disk-config.nix Add home/nixos-config/modules/nixos/files.nix Add home/nixos-config/modules/nixos/home-manager.nix Add home/nixos-config/modules/nixos/packages.nix Add home/nixos-config/modules/shared/README.md Add home/nixos-config/modules/shared/cachix/default.nix Add home/nixos-config/modules/shared/config/emacs/config.org Add home/nixos-config/modules/shared/config/emacs/.gitignore Add home/nixos-config/modules/shared/config/emacs/init.el Add home/nixos-config/modules/shared/config/p10k.zsh Add home/nixos-config/modules/shared/default.nix Add home/nixos-config/modules/shared/files.nix Add home/nixos-config/modules/shared/home-manager.nix Add home/nixos-config/modules/shared/packages.nix Add home/nixos-config/overlays/10-feather-font.nix Add home/nixos-config/overlays/README.md

---
+++ home/nixos-config/modules/darwin/casks.nix
@@ -0,0 +1,27 @@
+_:
+
+[
+  # Tier-1
+  "arc"
+  "chatgpt"
+  "raycast"
+
+  # Tier-2
+  "alt-tab"
+  "maccy"
+  "espanso"
+  "finicky"
+  "wezterm"
+
+  # Development Tools
+  # "visual-studio-code"
+
+  # Communication Tools
+  "telegram"
+  "zoom"
+
+  # Utility Tools
+  "syncthing"
+
+  # Entertainment Tools
+]
