1e7d9b8e05a137e11d934ef84fc1f8218f86feaf
Beforerr <zzj956959688@gmail.com>
2025-05-10 13:09:16 -0700

    Update home/.chezmoiscripts/install-cargo.sh Update home/scripts/julia.just

--- home/.chezmoiscripts/run_onchange_after_install-cargo.sh
+++ home/.chezmoiscripts/run_onchange_after_install-cargo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 command -v rustup || rustup default stable
+command -v cargo binstall || curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-if command -v cargo &> /dev/null; then
+if command -v cargo &>/dev/null; then
     cargo install garden-tools
+fi
+
+if command -v cargo binstall &>/dev/null; then
+    cargo binstall --strategies crate-meta-data jj-cli
 fi
