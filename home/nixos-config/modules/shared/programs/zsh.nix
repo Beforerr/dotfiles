38ba57c719afd6f30c425d998f4e6469cb83fce3
Beforerr <zzj956959688@gmail.com>
2026-06-09 18:34:40 -0700

    Update home/.claude/settings.json Update home/.zshrc Update home/nixos-config/modules/shared/programs/zsh.nix

--- home/nixos-config/modules/shared/programs/zsh.nix
+++ home/nixos-config/modules/shared/programs/zsh.nix
@@ -9,15 +9,6 @@
     syntaxHighlighting.enable = true;
     dotDir = ".config/zsh";
     initExtraFirst = "source $HOME/.zshrc";
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "plugins/aliases"; tags = ["from:oh-my-zsh"];}
-        { name = "plugins/brew"; tags = ["from:oh-my-zsh"];} # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/brew
-        { name = "plugins/git"; tags = ["from:oh-my-zsh"];}
-        { name = "hlissner/zsh-autopair"; tags = ["defer:2"];}
-        { name = "~/.config/zsh/plugins"; tags = ["from:local"];}
-      ];
-      };
-    };
+    completionInit = "";   # ~/.zshrc runs the (cached) compinit; suppress home-manager's
+  };
 }
