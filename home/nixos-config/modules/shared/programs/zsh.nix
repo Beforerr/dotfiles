0abe42601d7ba4cc0c6dbc050a428648e06cd256
Beforerr <zzj956959688@gmail.com>
2026-08-01 11:55:58 +0900

    Update home/nixos-config/modules/shared/programs/zsh.nix

--- home/nixos-config/modules/shared/programs/zsh.nix
+++ home/nixos-config/modules/shared/programs/zsh.nix
@@ -8,6 +8,9 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     dotDir = ".config/zsh";
+    envExtra = ''
+      export JULIA_MAX_NUM_PRECOMPILE_FILES=3
+    '';
     initExtraFirst = "source $HOME/.zshrc";
     completionInit = "";   # ~/.zshrc runs the (cached) compinit; suppress home-manager's
     profileExtra = ''eval "$(/opt/homebrew/bin/brew shellenv)"'';
